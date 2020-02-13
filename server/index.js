376568027f5877a92fb5c22438ecfa3c36489957
Sharofiddin Azizmatov <napster1202@gmail.com>
2020-02-13 15:48:10 +0200

    init server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require("./routes/api/posts");
+app.use("/api/posts", posts);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
